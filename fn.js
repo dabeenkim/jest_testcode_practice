1cb2b19adce3241bf1ee8e4e158fdae4f4483d14
dabeenkim <ekqls6812@naver.com>
2023-04-01 22:32:29 +0900

    [study]테스트코드연습

--- fn.js
+++ fn.js
@@ -1,5 +1,6 @@
 const fn = {
   add: (num1, num2) => num1 + num2,
+  makeUser: (name, age) => ({ name, age, gender: undifined }), //객체를 만들어주는 함수작성
 };
 
 module.exports = fn;
--- fn.test.js
+++ fn.test.js
@@ -1,17 +1,35 @@
 const fn = require("./fn");
 
-test("1은 1이야", () => {
-  expect(1).toBe(1);
+test("이름과 나이를 전달받아서 객체를 반환해줘", () => {
+  expect(fn.makeUser("Mike", 30)).toEqual({
+    name: "Mike",
+    age: 30,
+  });
 });
 
-test("2더하기 3은 5야", () => {
-  expect(fn.add(2, 3)).toBe(5);
+test("이름과 나이를 전달받아서 객체를 반환해줘", () => {
+  expect(fn.makeUser("Mike", 30)).toStrictEqual({
+    name: "Mike",
+    age: 30,
+  });
 });
 
-test("3더하기 3은 5가 이나야", () => {
-  expect(fn.add(3, 3)).not.toBe(5);
-});
+// test("1은 1이야", () => {
+//   expect(1).toBe(1);
+// });
 
-test("6더하기 3은 2가 아니야", () => {
-  expect(fn.add(6, 3)).not.toBe(5);
-});
+// test("2더하기 3은 5야", () => {
+//   expect(fn.add(2, 3)).toBe(5);
+// });
+
+// test("3더하기 3은 5가 이나야", () => {
+//   expect(fn.add(3, 3)).not.toBe(5);
+// });
+
+// test("6더하기 3은 2가 아니야", () => {
+//   expect(fn.add(6, 3)).not.toBe(5);
+// });
+
+// test("2더하기 3은 5야", () => {
+//   expect(fn.add(2, 3)).toEqual(5);
+// });
